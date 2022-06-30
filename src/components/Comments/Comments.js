f2eb51d0aa6cf6fb98bd2188419eca5460fecf54
stephonduong <stephonduong@gmail.com>
2022-06-29 22:43:28 -0400

    added comment section

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,10 +1,16 @@
 import './Comments.scss'
 import React from 'react';
+import avatarImage from '../../assets/images/Mohan-muruge.jpg'
 
-const Comments = () => {
+const Comments = (props) => {
     return (
-        <div>
-            
+        <div className="comments">
+            <img className="comments__image" src={avatarImage} alt="avatar"/>
+            <div className="comments__box">
+                <h4>join the conversation</h4>
+                <textarea className="comments__input" type="text" rows="4" placeholder="Add a new comment"/>
+                <button className="comments__button">comment</button>
+            </div>
         </div>
     );
 };
