1f828716434de579803e120b57488bcd952dbc08
stephonduong <stephonduong@gmail.com>
2022-07-01 14:29:43 -0400

    pulled comments

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -11,8 +11,8 @@ const Comments = (props) => {
         <img className="comments__image" src={avatarImage} alt="avatar"/>
         <div className="comments__box">
           <div className="comments__tablet-input-display">
-          <h3 className="comments__header">join the conversation</h3>
-          <textarea className="comments__input" type="text" placeholder="Add a new comment"/>
+            <h3 className="comments__header">join the conversation</h3>
+            <textarea className="comments__input" type="text" placeholder="Add a new comment"/>
           </div>
           <button className="comments__button">comment</button>
         </div>
